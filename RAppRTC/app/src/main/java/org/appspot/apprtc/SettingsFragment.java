6c802f11d5c6771f1b7a12ec1c7c8dd8d1b1d52b
RWebRTC <rwebrtc@gmail.com>
2016-09-14 11:35:37 +0800

    设置：从配置文件中读属性并初始化系统

--- RAppRTC/app/src/main/java/org/appspot/apprtc/SettingsFragment.java
+++ RAppRTC/app/src/main/java/org/appspot/apprtc/SettingsFragment.java
@@ -18,10 +18,10 @@ import android.preference.PreferenceFragment;
  */
 public class SettingsFragment extends PreferenceFragment {
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // Load the preferences from an XML resource
-    addPreferencesFromResource(R.xml.preferences);
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
