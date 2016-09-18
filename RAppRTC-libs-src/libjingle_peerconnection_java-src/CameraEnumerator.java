1860c5d354059f53c57113ee4f463daf512f5bac
RWebRTC <rwebrtc@gmail.com>
2016-09-18 10:55:39 +0800

    add Enumerator.java: 相机枚举

--- RAppRTC-libs-src/libjingle_peerconnection_java-src/CameraEnumerator.java
+++ RAppRTC-libs-src/libjingle_peerconnection_java-src/CameraEnumerator.java
@@ -10,11 +10,28 @@
 
 package org.webrtc;
 
+/**
+ * 相机枚举
+ */
 public interface CameraEnumerator {
-  public String[] getDeviceNames();
-  public boolean isFrontFacing(String deviceName);
-  public boolean isBackFacing(String deviceName);
+    public String[] getDeviceNames();
 
-  public CameraVideoCapturer createCapturer(String deviceName,
-      CameraVideoCapturer.CameraEventsHandler eventsHandler);
+    /**
+     * 是否是前置摄像头
+     *
+     * @param deviceName 硬件名称
+     * @return 是否
+     */
+    public boolean isFrontFacing(String deviceName);
+
+    /**
+     * 是否是后置摄像头
+     *
+     * @param deviceName 硬件名称
+     * @return 是否
+     */
+    public boolean isBackFacing(String deviceName);
+
+    public CameraVideoCapturer createCapturer(String deviceName,
+                                              CameraVideoCapturer.CameraEventsHandler eventsHandler);
 }
